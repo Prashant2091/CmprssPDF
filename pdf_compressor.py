4d5ba50c901945cc3811bf223bf57dd5aa3df180
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 13:17:52 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import base64
 import tempfile
-import PyPDF2
+import PyPDF4
 
 # Function to compress the PDF
 def compress_pdf(uploaded_file, compression_factor):
-    pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
-    pdf_writer = PyPDF2.PdfFileWriter()
+    pdf_reader = PyPDF4.PdfReader(uploaded_file)
+    pdf_writer = PyPDF4.PdfWriter()
 
-    for page_number in range(pdf_reader.getNumPages()):
-        page = pdf_reader.getPage(page_number)
-        page.compressContentStreams(compression_factor)
-        pdf_writer.addPage(page)
+    for page in pdf_reader.pages:
+        pdf_writer.add_page(page)
 
-    return pdf_writer
+    # Save the compressed PDF to a temporary file
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    with open(temp_file.name, 'wb') as f:
+        pdf_writer.write(f)
+
+    return temp_file
 
 # Streamlit app title
 st.title('PDF Compressor')
@@ -30,17 +33,12 @@ if uploaded_file is not None:
 
     if st.button("Compress PDF"):
         # Compress the PDF
-        compressed_pdf = compress_pdf(uploaded_file, compression_factor)
-
-        # Save the compressed PDF to a temporary file
-        temp_file = tempfile.NamedTemporaryFile(delete=False)
-        with open(temp_file.name, 'wb') as f:
-            compressed_pdf.write(f)
+        compressed_file = compress_pdf(uploaded_file, compression_factor)
 
         # Display the compressed file size
-        st.write(f"Compressed File Size: {round(temp_file.stat().st_size / 1024, 2)} KB")
+        st.write(f"Compressed File Size: {round(compressed_file.stat().st_size / 1024, 2)} KB")
 
         # Download the compressed PDF
-        with open(temp_file.name, "rb") as f:
+        with open(compressed_file.name, "rb") as f:
             data = f.read()
         st.download_button(label="Download Compressed PDF", data=data, file_name="compressed_pdf.pdf")
