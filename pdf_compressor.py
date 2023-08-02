45e47ff797e14f3203131916cf1290121e6e2911
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 22:49:24 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -9,7 +9,18 @@ def compress_pdf(uploaded_file, compression_factor=0.5):
 
     for page_number in range(pdf_reader.getNumPages()):
         page = pdf_reader.getPage(page_number)
-        page.compressContentStreams(compression_factor)
+        content = page.getContents()
+        if not isinstance(content, PyPDF2.pdf.ContentStream):
+            content = PyPDF2.pdf.ContentStream(content, pdf_writer)
+        for operands, operator in content.operations:
+            if operator == b'Tj':
+                # Text-showing operators (Tj) represent text content
+                operands[0] = PyPDF2.generic.TextStringObject(operands[0].get_object().encode('ascii', 'ignore').decode())
+            elif operator == b'TJ':
+                # Text-showing operators (TJ) represent array of strings
+                operands[0] = PyPDF2.generic.ArrayObject([PyPDF2.generic.TextStringObject(obj.get_object().encode('ascii', 'ignore').decode()) if isinstance(obj, PyPDF2.generic.TextStringObject) else obj for obj in operands[0]])
+        page.__setitem__(PyPDF2.generic.NameObject('/Contents'), content)
+        page.compressContentStreams()
         pdf_writer.addPage(page)
 
     compressed_pdf = io.BytesIO()
