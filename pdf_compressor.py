dbdf7d0255cb9b8a4418d8776a8ed46a1c2dff3f
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 19:56:20 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,32 +1,47 @@
 import streamlit as st
-import base64
 import PyPDF2
 import io
 
-def compress_pdf(input_file, compression_factor=0.5):
-    pdf_reader = PyPDF2.PdfFileReader(input_file)
-    compressed_pdf = io.BytesIO()
-    pdf_writer = PyPDF2.PdfFileWriter()
+def compress_pdf(uploaded_file, compression_factor=0.5):
+    pdf_reader = PyPDF2.PdfReader(uploaded_file)
+    output_buffer = io.BytesIO()
+
+    pdf_writer = PyPDF2.PdfWriter()
 
     for page in pdf_reader.pages:
         compressed_page = page.compress(compression_factor)
         pdf_writer.add_page(compressed_page)
 
-    pdf_writer.write(compressed_pdf)
-    compressed_pdf.seek(0)
-    return compressed_pdf
+    pdf_writer.write(output_buffer)
+    return output_buffer
 
 def main():
     st.title("PDF Compressor")
 
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-    if uploaded_file:
-        compression_factor = st.slider("Select Compression Factor", min_value=0.1, max_value=1.0, step=0.1, value=0.5)
+
+    if uploaded_file is not None:
+        st.subheader("Original File Size:")
+        st.write(f"{uploaded_file.size / 1024:.2f} KB")
+
+        compression_factor = st.slider("Compression Factor", 0.1, 1.0, 0.5, 0.1)
 
         if st.button("Compress PDF"):
             compressed_pdf = compress_pdf(uploaded_file, compression_factor)
 
-            st.markdown(f'<a href="data:application/pdf;base64,{base64.b64encode(compressed_pdf.getvalue()).decode()}" download="compressed.pdf">Download Compressed PDF</a>', unsafe_allow_html=True)
+            st.subheader("Compressed File Size:")
+            st.write(f"{compressed_pdf.getbuffer().nbytes / 1024:.2f} KB")
+
+            st.markdown(
+                f'<a href="data:application/pdf;base64,{base64.b64encode(compressed_pdf.getvalue()).decode()}" download="compressed.pdf">Download Compressed PDF</a>',
+                unsafe_allow_html=True
+            )
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
