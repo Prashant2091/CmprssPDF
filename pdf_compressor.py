fdee532db2248e2991fcba48a0c2b3cd0b80dad2
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 16:04:21 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import PyPDF2
-import base64
-from io import BytesIO
+import io
+from reportlab.pdfgen import canvas
 
 # Function to compress the PDF
 def compress_pdf(input_file, compression_factor):
-    output_file = BytesIO()
+    output_file = io.BytesIO()
 
     with input_file as file:
         reader = PyPDF2.PdfFileReader(file)
@@ -13,8 +13,14 @@ def compress_pdf(input_file, compression_factor):
 
         for page_num in range(reader.getNumPages()):
             page = reader.getPage(page_num)
-            page.compressContentStreams()
-            writer.addPage(page)
+            compressed_page = io.BytesIO()
+            c = canvas.Canvas(compressed_page)
+            c.setPageSize((page.mediaBox.getWidth() / compression_factor, page.mediaBox.getHeight() / compression_factor))
+            c.doFormXObject(PyPDF2.pdf.ContentStream(page.getContents(), page.pdf))
+            c.save()
+            compressed_page.seek(0)
+            compressed_pdf_page = PyPDF2.PdfFileReader(compressed_page)
+            writer.addPage(compressed_pdf_page.getPage(0))
 
         writer.write(output_file)
         output_file.seek(0)
@@ -31,13 +37,12 @@ if uploaded_file is not None:
     st.write("Original File Size:", round(uploaded_file.size / 1024, 2), "KB")
 
     # Compression factor slider
-    compression_factor = st.slider("Select Compression Factor", min_value=0, max_value=9, value=4)
+    compression_factor = st.slider("Select Compression Factor", min_value=1, max_value=10, value=4)
 
     # Compress the PDF and get the compressed file
     compressed_file = compress_pdf(uploaded_file, compression_factor)
 
     st.write("Download Compressed PDF")
-    compressed_file_base64 = base64.b64encode(compressed_file.read()).decode("utf-8")
-    st.markdown(f'<a href="data:application/pdf;base64,{compressed_file_base64}" download="compressed_pdf.pdf">Click here to download</a>', unsafe_allow_html=True)
+    st.download_button("Click here to download", data=compressed_file, file_name="compressed_pdf.pdf")
 
-    st.write("Compressed File Size:", round(len(compressed_file_base64) / 1024, 2), "KB")
+    st.write("Compressed File Size:", round(len(compressed_file.read()) / 1024, 2), "KB")
