6862368b1c97f31aee1e76deffabd14de6a28b2c
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 20:41:20 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -3,18 +3,22 @@ import PyPDF2
 import io
 import base64
 
+def compress_page(page, compression_factor):
+    content = page.get_object()
+    content.compress(compression_factor)
+    return page
+
 def compress_pdf(uploaded_file, compression_factor=0.5):
     pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
     pdf_writer = PyPDF2.PdfFileWriter()
 
     for page in pdf_reader.pages:
-        compressed_page = page.compress(compression_factor)
+        compressed_page = compress_page(page, compression_factor)
         pdf_writer.add_page(compressed_page)
 
-    output_buffer = io.BytesIO()
-    pdf_writer.write(output_buffer)
-
-    return output_buffer
+    compressed_pdf = io.BytesIO()
+    pdf_writer.write(compressed_pdf)
+    return compressed_pdf
 
 def main():
     st.title("PDF Compressor")
@@ -31,7 +35,7 @@ def main():
             compressed_pdf = compress_pdf(uploaded_file, compression_factor)
 
             st.subheader("Compressed File Size:")
-            st.write(f"{len(compressed_pdf.getbuffer()) / 1024:.2f} KB")
+            st.write(f"{len(compressed_pdf.getvalue()) / 1024:.2f} KB")
 
             st.markdown(
                 f'<a href="data:application/pdf;base64,{base64.b64encode(compressed_pdf.getvalue()).decode()}" download="compressed.pdf">Download Compressed PDF</a>',
@@ -40,3 +44,8 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
