7f8c8d9d3db2abfecc1312618150fadfd8f32b5d
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 22:31:26 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,27 +1,30 @@
-import io
-import PyPDF2
 import streamlit as st
+import PyPDF2
+import io
 import base64
-def compress_pdf(uploaded_file):
+
+def compress_pdf(uploaded_file, compression_factor=0.5):
     pdf_reader = PyPDF2.PdfFileReader(uploaded_file)
     pdf_writer = PyPDF2.PdfFileWriter()
 
-    for page_number in range(len(pdf_reader.pages)):
-        page = pdf_reader.getPage(page_number)
-        page.compressContentStreams()
-        pdf_writer.addPage(page)
+    for page in pdf_reader.pages:
+        pdf_writer.add_page(page)
 
-    compressed_pdf_bytes = io.BytesIO()
-    pdf_writer.write(compressed_pdf_bytes)
-    return compressed_pdf_bytes
+    compressed_pdf = io.BytesIO()
+    pdf_writer.write(compressed_pdf)
+    compressed_pdf.seek(0)
+
+    return compressed_pdf
 
 def main():
     st.title("PDF Compressor")
 
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
-    if uploaded_file is not None:
-        if st.button("Compress PDF"):
-            compressed_pdf = compress_pdf(uploaded_file)
+    if uploaded_file:
+        compression_factor = st.slider("Compression Factor", 0.0, 1.0, 0.5, 0.01)
+
+        if st.button("Compress"):
+            compressed_pdf = compress_pdf(uploaded_file, compression_factore)
 
             st.markdown(
                 f'<a href="data:application/pdf;base64,{base64.b64encode(compressed_pdf.getvalue()).decode()}" download="compressed.pdf">Download Compressed PDF</a>',
