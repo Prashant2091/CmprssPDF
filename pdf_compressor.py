84b67c584f8c88332bf8b7b896d023b5cb3c966c
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 13:21:46 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,22 +1,17 @@
 import streamlit as st
-import base64
-import tempfile
-import PyPDF4
+from PyPDF2 import PdfReader, PdfWriter
 
 # Function to compress the PDF
 def compress_pdf(uploaded_file, compression_factor):
-    pdf_reader = PyPDF4.PdfReader(uploaded_file)
-    pdf_writer = PyPDF4.PdfWriter()
+    pdf_reader = PdfReader(uploaded_file)
+    pdf_writer = PdfWriter()
 
     for page in pdf_reader.pages:
+        page.compressContentStreams(compression_factor)
         pdf_writer.add_page(page)
 
-    # Save the compressed PDF to a temporary file
-    temp_file = tempfile.NamedTemporaryFile(delete=False)
-    with open(temp_file.name, 'wb') as f:
-        pdf_writer.write(f)
-
-    return temp_file
+    output_buffer = st._upload_file_manager._get_encoded_file_contents(pdf_writer)
+    return output_buffer
 
 # Streamlit app title
 st.title('PDF Compressor')
@@ -28,17 +23,15 @@ if uploaded_file is not None:
     # Display the original file size
     st.write(f"Original File Size: {round(len(uploaded_file.read()) / 1024, 2)} KB")
 
-    # Compression factor
+    # Compression factor (this will affect image quality)
     compression_factor = st.slider("Select Compression Factor:", min_value=1, max_value=10, step=1, value=2)
 
     if st.button("Compress PDF"):
         # Compress the PDF
-        compressed_file = compress_pdf(uploaded_file, compression_factor)
+        compressed_pdf = compress_pdf(uploaded_file, compression_factor)
 
         # Display the compressed file size
-        st.write(f"Compressed File Size: {round(compressed_file.stat().st_size / 1024, 2)} KB")
+        st.write(f"Compressed File Size: {round(len(compressed_pdf) / 1024, 2)} KB")
 
         # Download the compressed PDF
-        with open(compressed_file.name, "rb") as f:
-            data = f.read()
-        st.download_button(label="Download Compressed PDF", data=data, file_name="compressed_pdf.pdf")
+        st.download_button(label="Download Compressed PDF", data=compressed_pdf, file_name="compressed_pdf.pdf")
