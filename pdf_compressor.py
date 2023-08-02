bf2f26a0482f0f8c7d1b6f6803a4d972c36546cf
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 19:53:19 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,21 +1,19 @@
 import streamlit as st
+import base64
 import PyPDF2
 import io
 
-def compress_pdf(input_file):
-    pdf_reader = PyPDF2.PdfReader(input_file)
-    pdf_writer = PyPDF2.PdfWriter()
+def compress_pdf(input_file, compression_factor=0.5):
+    pdf_reader = PyPDF2.PdfFileReader(input_file)
+    compressed_pdf = io.BytesIO()
+    pdf_writer = PyPDF2.PdfFileWriter()
 
     for page in pdf_reader.pages:
-        pdf_writer.add_page(page)
-    
-    # Set compression options for content streams
-    pdf_writer.compressContentStreams(compression_level=5)
-    
-    compressed_pdf = io.BytesIO()
+        compressed_page = page.compress(compression_factor)
+        pdf_writer.add_page(compressed_page)
+
     pdf_writer.write(compressed_pdf)
     compressed_pdf.seek(0)
-    
     return compressed_pdf
 
 def main():
@@ -23,9 +21,12 @@ def main():
 
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
     if uploaded_file:
-        compressed_pdf = compress_pdf(uploaded_file)
+        compression_factor = st.slider("Select Compression Factor", min_value=0.1, max_value=1.0, step=0.1, value=0.5)
 
-        st.markdown(f'<a href="data:application/pdf;base64,{base64.b64encode(compressed_pdf.getvalue()).decode()}" download="compressed.pdf">Download Compressed PDF</a>', unsafe_allow_html=True)
+        if st.button("Compress PDF"):
+            compressed_pdf = compress_pdf(uploaded_file, compression_factor)
+
+            st.markdown(f'<a href="data:application/pdf;base64,{base64.b64encode(compressed_pdf.getvalue()).decode()}" download="compressed.pdf">Download Compressed PDF</a>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
