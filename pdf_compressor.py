e3f0bcb02b566a0bb0405bb168d202095f81de40
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 15:24:47 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,21 +1,41 @@
 import streamlit as st
 import PyPDF2
 import io
+import img2pdf
+import subprocess
 
 def compress_pdf(input_file, compression_factor):
+    # Convert the PDF pages to images and compress them using ImageMagick
+    images = []
     reader = PyPDF2.PdfFileReader(input_file)
-    writer = PyPDF2.PdfFileWriter()
+    temp_files = []
 
     for page_number in range(reader.numPages):
         page = reader.getPage(page_number)
-        # Reduce the resolution to reduce file size
-        page.compressContentStreams(compression_factor)
-        writer.addPage(page)
+        pdf_page_bytes = io.BytesIO()
+        pdf_writer = PyPDF2.PdfFileWriter()
+        pdf_writer.addPage(page)
+        pdf_writer.write(pdf_page_bytes)
+        pdf_page_bytes.seek(0)
 
-    output_buffer = io.BytesIO()
-    writer.write(output_buffer)
-    output_buffer.seek(0)
-    return output_buffer
+        image_file = io.BytesIO()
+        subprocess.run(['convert', '-density', '300', '-', '-quality', str(compression_factor * 100), 'JPEG:-'],
+                       input=pdf_page_bytes.read(), stdout=image_file, check=True, text=True)
+        image_file.seek(0)
+
+        temp_files.append(image_file)
+        images.append(image_file)
+
+    # Create a PDF from the compressed images
+    compressed_pdf_bytes = io.BytesIO()
+    img2pdf.convert(images, outputstream=compressed_pdf_bytes)
+    compressed_pdf_bytes.seek(0)
+
+    # Close the temporary image files
+    for temp_file in temp_files:
+        temp_file.close()
+
+    return compressed_pdf_bytes
 
 st.title("PDF Compressor")
 
