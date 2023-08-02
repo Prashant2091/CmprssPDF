7b97a54d51bc4a124ac69f7ba82ab24c60033c48
Prashant Shukla <97425883+Prashant2091@users.noreply.github.com>
2023-08-02 15:05:07 +0530

    Update pdf_compressor.py

--- pdf_compressor.py
+++ pdf_compressor.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import PyPDF2
+import tempfile
+import os
 
 def compress_pdf(input_file, output_file, compression_factor):
     with open(input_file, "rb") as file:
@@ -27,15 +29,23 @@ if uploaded_file is not None:
     # Slider for compression factor
     compression_factor = st.slider("Select Compression Factor", min_value=0.1, max_value=1.0, step=0.1, value=0.5)
 
+    # Save the uploaded PDF to a temporary file
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    temp_file.write(uploaded_file.read())
+    temp_file.close()
+
     # Compress the PDF file
     compressed_file = f"compressed_{uploaded_file.name}"
-    compress_pdf(uploaded_file, compressed_file, compression_factor)
+    compress_pdf(temp_file.name, compressed_file, compression_factor)
 
     # Display the compressed file size
-    compressed_size = round(len(open(compressed_file, "rb").read()) / 1024, 2)  # Convert to KB
+    compressed_size = round(os.path.getsize(compressed_file) / 1024, 2)  # Convert to KB
     st.write(f"Download Compressed PDF (Compression Factor: {compression_factor:.1f})")
     st.download_button(
         label=f"Compressed File Size: {compressed_size} KB",
         data=open(compressed_file, "rb").read(),
         file_name=compressed_file,
     )
+
+    # Remove the temporary file
+    os.remove(temp_file.name)
